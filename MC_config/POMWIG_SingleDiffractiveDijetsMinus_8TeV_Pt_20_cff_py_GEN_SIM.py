df7319ffe2656d5fe7de670efd69ba9a379534e7
sandrofonseca <sfonseca@Sandro-Fonseca-de-Souza-macbook-pro.local>
2014-01-22 10:39:17 -0200

    pomwig MC config files

---
+++ MC_config/POMWIG_SingleDiffractiveDijetsMinus_8TeV_Pt_20_cff_py_GEN_SIM.py
@@ -0,0 +1,73 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.372.2.14 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
+# with command line options: GeneratorInterface/PomwigInterface/python/POMWIG_SingleDiffractiveDijetsMinus_8TeV_Pt_20_cff.py --step GEN,SIM --beamspot Realistic8TeVCollision --conditions START52_V9::All --pileup NoPileUp --datamix NODATAMIXER --eventcontent RAWSIM --datatier GEN-SIM -n 10
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('SIM')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedRealistic8TeVCollision_cfi')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('Configuration.StandardSequences.SimIdeal_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(10)
+)
+
+# Input source
+process.source = cms.Source("EmptySource")
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.372.2.14 $'),
+    annotation = cms.untracked.string('GeneratorInterface/PomwigInterface/python/POMWIG_SingleDiffractiveDijetsMinus_8TeV_Pt_20_cff.py nevts:10'),
+    name = cms.untracked.string('PyReleaseValidation')
+)
+
+# Output definition
+
+process.RAWSIMoutput = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    outputCommands = process.RAWSIMEventContent.outputCommands,
+    fileName = cms.untracked.string('POMWIG_SingleDiffractiveDijetsMinus_8TeV_Pt_20_cff_py_GEN_SIM.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('GEN-SIM')
+    ),
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    )
+)
+
+# Additional output definition
+
+# Other statements
+process.GlobalTag.globaltag = 'START52_V9::All'
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(process.pgen)
+process.simulation_step = cms.Path(process.psim)
+process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.RAWSIMoutput_step = cms.EndPath(process.RAWSIMoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.simulation_step,process.endjob_step,process.RAWSIMoutput_step)
+
---
+++ MC_config/POMWIG_SingleDiffractiveZmumuPlus_8TeV_M10_cff_py_GEN_SIM.py
@@ -0,0 +1,107 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.372.2.14 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
+# with command line options: GeneratorInterface/PomwigInterface/python/POMWIG_SingleDiffractiveZmumuPlus_8TeV_M10_cff.py --step GEN,SIM --beamspot Realistic8TeVCollision --conditions START52_V9::All --pileup NoPileUp --datamix NODATAMIXER --eventcontent RAWSIM --datatier GEN-SIM -n 10
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('SIM')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedRealistic8TeVCollision_cfi')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('Configuration.StandardSequences.SimIdeal_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(10)
+)
+
+# Input source
+process.source = cms.Source("EmptySource")
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.372.2.14 $'),
+    annotation = cms.untracked.string('GeneratorInterface/PomwigInterface/python/POMWIG_SingleDiffractiveZmumuPlus_8TeV_M10_cff.py nevts:10'),
+    name = cms.untracked.string('PyReleaseValidation')
+)
+
+# Output definition
+
+process.RAWSIMoutput = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    outputCommands = process.RAWSIMEventContent.outputCommands,
+    fileName = cms.untracked.string('POMWIG_SingleDiffractiveZmumuPlus_8TeV_M10_cff_py_GEN_SIM.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('GEN-SIM')
+    ),
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    )
+)
+
+# Additional output definition
+
+# Other statements
+process.GlobalTag.globaltag = 'START52_V9::All'
+
+process.generator = cms.EDFilter("PomwigGeneratorFilter",
+    doMPInteraction = cms.bool(False),
+    useJimmy = cms.bool(False),
+    herwigHepMCVerbosity = cms.untracked.bool(False),
+    filterEfficiency = cms.untracked.double(1.0),
+    herwigVerbosity = cms.untracked.int32(1),
+    comEnergy = cms.double(8000.0),
+    printCards = cms.untracked.bool(True),
+    crossSection = cms.untracked.double(-1.0),
+    maxEventsToPrint = cms.untracked.int32(2),
+    survivalProbability = cms.double(0.05),
+    HerwigParameters = cms.PSet(
+        SD2InclusiveZmumu = cms.vstring('NSTRU      = 14         ! H1 Pomeron Fit B', 
+            'Q2WWMN     = 1E-6       ! Minimum |t|', 
+            'Q2WWMX     = 4.0        ! Maximum |t|', 
+            'YWWMIN     = 1E-6       ! Minimum xi', 
+            'YWWMAX     = 0.2        ! Maximum xi', 
+            'IPROC      = 11353      ! Process PomP -> Z/gamma*->mumu', 
+            'EMMIN      = 10         ! Minimum DY mass', 
+            'MODPDF(2)  = -1         ! Set MODPDF', 
+            'MODPDF(1)  = 10150      ! Set MODPDF CTEQ61'),
+        parameterSets = cms.vstring('SD2InclusiveZmumu')
+    ),
+    h1fit = cms.int32(2),
+    doPDGConvert = cms.bool(False),
+    diffTopology = cms.int32(2)
+)
+
+
+process.ProductionFilterSequence = cms.Sequence(process.generator)
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(process.pgen)
+process.simulation_step = cms.Path(process.psim)
+process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.RAWSIMoutput_step = cms.EndPath(process.RAWSIMoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.simulation_step,process.endjob_step,process.RAWSIMoutput_step)
+# filter all path with the production filter sequence
+for path in process.paths:
+	getattr(process,path)._seq = process.ProductionFilterSequence * getattr(process,path)._seq 
+
